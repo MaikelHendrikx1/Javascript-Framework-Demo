eecafe2da0dbc9a0a2470d48d66c81c5ef75c4e8
Maikel Hendrikx <464318@student.fontys.nl>
2022-02-10 15:31:36 +0100

    first commit

---
+++ Svelte/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	let catFacts = [];
+
+	function GenerateCatFact(){
+		fetch('https://catfact.ninja/fact')
+    	.then((response) => response.json())
+    	.then((responseJSON) => {
+			catFacts.push(responseJSON.fact);
+			catFacts = catFacts;
+    	});
+	}
+	
+</script>
+
+<main>
+	{#each catFacts as catFact}
+		<h2>{catFact}</h2>
+		<hr>
+	{/each}
+
+	<button on:click={GenerateCatFact}>Generate a cat fact!</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		max-width: none;
+	}
+</style>
